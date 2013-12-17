12f327ab6aa3e7f5743db6e53632cfb0d86abc1e
Jesse Glick <jglick@cloudbees.com>
2013-12-17 17:43:41 -0500

    [JENKINS-21052] So far unsuccessful attempt to make l:copyButton work.

--- core/src/main/resources/lib/layout/copyButton/copyButton.js
+++ core/src/main/resources/lib/layout/copyButton/copyButton.js
@@ -5,33 +5,31 @@ Behaviour.specify("span.copy-button", 'copyButton', 0, function(e) {
         var id = "copy-button"+(iota++);
         btn.id = id;
 
-        var clip = new ZeroClipboard.Client();
-        clip.setText(e.getAttribute("text"));
+        var clip = new ZeroClipboard({forceHandCursor: true});
         makeButton(btn);
-        clip.setHandCursor(true);
 
         var container = e.getAttribute("container");
         if (container) {
             container = $(e).up(container);
             container.style.position = "relative";
-            clip.glue(id,container);
+            clip.glue(btn, container);
         } else {
-            clip.glue(id);
+            clip.glue(btn);
         }
 
-        clip.addEventListener('onComplete',function() {
+        clip.on('complete',function() {
             notificationBar.show(e.getAttribute("message"));
         });
-        clip.addEventListener('onMouseOver',function() {
+        clip.on('mouseover',function() {
           $(id).addClassName('yui-button-hover')
         });
-        clip.addEventListener('onMouseOut',function() {
+        clip.on('mouseout',function() {
             $(id).removeClassName('yui-button-hover')
         });
-        clip.addEventListener('onMouseDown',function() {
+        clip.on('mousedown',function() {
             $(id).addClassName('yui-button-active')
         });
-        clip.addEventListener('onMouseUp',function() {
+        clip.on('mouseup',function() {
             $(id).removeClassName('yui-button-active')
         });
 });
